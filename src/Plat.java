4297443582dd0f5eaae6581514c9ffb1d866c32e
hiba <hibazekraoui23@email.com>
2025-11-21 16:50:05 +0100

    abdellah added plat et plat special

---
+++ src/Plat.java
@@ -0,0 +1,12 @@
+public class Plat {
+    int id;
+    String nomDePlat;
+    double prix;
+
+    Plat(int id, String nomDePlat, double prix) {
+        this.id = id;
+        this.prix = prix;
+        this.nomDePlat = nomDePlat;
+    }
+
+}
