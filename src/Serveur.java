db920fb1d8710826369d65278131e4181fbac262
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2025-11-24 09:14:45 +0100

    maintain ServeurVIP class

--- src/Serveur.java
+++ src/Serveur.java
@@ -11,12 +11,3 @@ public class Serveur {
         this.commandes = new ArrayList<>();
     }
 }
-public class ServeurVIP extends Serveur{
-    public ServeurVIP(int id) {
-        super(id);
-    }
-
-    public static void serviceVIP(){
-        System.out.println("Welcome to VIP service (^^)");
-    }
-}
--- src/ServeurVIP.java
+++ src/ServeurVIP.java
@@ -1,2 +1,9 @@
-public class ServeurVIP {
+public class ServeurVIP extends Serveur{
+    public ServeurVIP(int id) {
+        super(id);
+    }
+
+    public static void serviceVIP(){
+        System.out.println("Welcome to VIP service (^^)");
+    }
 }
